98366d6c69e827390627a9f2661a0beebdd918d1
brower54 <kildeev.zaur@yandex.ru>
2021-02-23 23:49:17 +0300

    [+] Change settings.py

--- simple_web_server/simple_web_server/settings.py
+++ simple_web_server/simple_web_server/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine(
+    "postgresql://admin:prosto100@localhost/main_web_db", echo=True)
+
+session = scoped_session(
+    sessionmaker(bind=engine)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'webapp',
 ]
 
 MIDDLEWARE = [
